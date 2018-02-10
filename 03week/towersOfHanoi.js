60077d122dbb97b525494b9dad70422a430708c3
Wade Cosby <wadecosby@Wades-MacBook-Pro.local>
2018-02-09 22:22:55 -0600

    adds test case functions for moving the stacks

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,5 +1,43 @@
 'use strict';
 
+/*
+***WHITEBOARD***
+TOWERS OF HANOI
+OBJECTIVE: Move the tower from stack a to stack c, allowing the stack to only be ordered in descending orders [3,2,1], and report a win
+
+GAME RULES:
+1. Move only one number at a time.
+2. A larger number may not be placed ontop of a smaller number.
+3. All numbers, except the one being moved, must be on a peg.
+
+Things the game does:
+1. User inputs a beginning stack (from) and ending stack (destination)
+    a. test the input from the user to verify the input is one of the allowed strings
+2. If the destination stack's last value is < beginning stack, then the value is placed at the end of the destination stack
+    a. else, the user is returned a message letting them know the move was illegal
+3. When stack c has a length of 4 items in descending order, the game will 
+        i. return a win
+        ii. reset the board
+    a. Else, the user will continue playing until a win is reported
+
+
+Variables:
+- validStackInputs: [array] - holds the valid inputs allowed 
+- lastValueOfStartStack - holds the last value of the starting stack array
+
+Functions:
+towersOfHanoi accepts a startStack and endStack from the user.  If the input matches the valid inputs, then test if the inputs are legal
+
+isLegal accepts inputs from towersOfHanoi and should validate that the array is either empty or the last value of the endingStack is greater than the startStack
+  if true, then allow the move (pass the start and end to movePiece)
+  else, report the illegal move to the user
+
+movePiece accepts the inputs from isLegal, pops the last value of the startStack to the lastValueOfStartStack and pushes the popped value to the end of the endStack
+
+*/
+
+
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -21,18 +59,21 @@ const printStacks = () => {
   console.log("c: " + stacks.c);
 }
 
-const movePiece = () => {
-  // Your code here
-
+const movePiece = (startStack, endStack) => {
+  const lastRing = stacks[startStack].pop()
+ return stacks[endStack].push(lastRing)
 }
 
-const isLegal = () => {
-  // Your code here
-
+const isLegal = (startStack, endStack) => {
+  return stacks[endStack][endStack.length] === undefined  || stacks[startStack][stacks[startStack].length-1] < stacks[endStack][stacks[endStack].length-1]
+  ? movePiece(startStack, endStack)
+  : console.log('nope')
 }
 
-const checkForWin = () => {
-  // Your code here
+const checkForWin = (startStack, endStack) => {
+ return stacks.c.length !== 4 
+  ? isLegal(startStack, endStack)
+  : console.log ("you win!")
 }
 
 /*
@@ -46,12 +87,11 @@ const inputLetterTest = (inputArr, inputStack) => {
 }
 
 const towersOfHanoi = (startStack, endStack) => {
-  (inputLetterTest(validStackInputs, startStack) && 
-    inputLetterTest(validStackInputs, endStack)) 
-   ? checkForWin()
+ return (inputLetterTest(validStackInputs, startStack) && 
+        inputLetterTest(validStackInputs, endStack)) 
+   ? checkForWin(startStack, endStack)
    : console.log("Input is invalid")
  }
-
 
 function getPrompt() {
   printStacks();
